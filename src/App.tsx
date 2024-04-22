efa65e208d42a2043119b4c6e3529fe02599c9aa
evgeniyko99 <ekk4357@gmail.com>
2024-04-22 16:00:02 -0400

    repository initialized

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+const App = () => {
+  return (
+    <>Hello</>
+  )
+}
+
+export default App
---
+++ src/components/Home.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+    return(
+        <div>
+            <p>Hi, I am</p>
+            <p>Evgeniy Ko</p>
+            <p>I'm a passionate software engineer dedicated to crafting innovative solutions with my extensive expertise in various programming languages.</p>
+        </div>
+    )
+}
+
+export default Home
